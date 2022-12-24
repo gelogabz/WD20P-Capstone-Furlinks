c72d848f35c441f506972810d494f08feb6443ed
harvz47 <86585814+harvz47@users.noreply.github.com>
2022-12-24 18:12:39 +0800

    Update editdog.blade.php
    updating

--- resources/views/pages/editdog.blade.php
+++ resources/views/pages/editdog.blade.php
@@ -88,64 +88,6 @@
           </tr>  
         </table>
       </div>
-
-      <!--Foster profile information -->
-      <div class="col-lg-3 col-sm-6" style="padding-top:10px;padding-right:20px;padding-left: 20px;">
-        <div class="border" style="border-radius:20px;padding:20px 20px 10px 20px">
-            <div class="row" style="padding-bottom:5px">
-                <div class="col col-auto" style="margin-right:px">
-                    <img style="width:55px; border-radius:50%; padding: 2px" src="{{asset('build/images/profilepic/chef.jpg') }}">
-                </div>
-                <div class="col">
-                    <div class="row" style="padding-top: 5px;padding-bottom: 5px;font-size:medium"><a href="profile.html"> @chefarnold </a></div>
-                    <div class="row" style="font-size: small;color:gray;vertical-align: bottom;padding-right: 2px;">5.0  
-                    <span style="padding-left:2px" class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>
-                    <span class="fa fa-star checked"></span>(20 reviews)
-                    </div>
-                </div>
-            </div>
-            <p style="font-size:13px;line-height:1.6; padding-top:15px">I am a chef who loves to take care of dogs and cats. During my free time, I go around our town to feed stray dogs. Some of my dogs are retired K9 and some are stray dogs that we rescued and took home.</p>
-            <p style="font-size:15px;color:#581542"><i>More dogs posted by @chefarnold</i></p> 
-            <div class="row" style="margin-bottom:15px">
-                <div class="col col-sm-6 col-xs-4" style="margin-bottom:20px;margin-right: 0px;margin-left: 0px;">
-                    <div class="containerimg" style="width:100%">
-                        <img src="{{asset('build/images/searchres/fem0.jpg') }}" alt="Avatar" class="image" style="width:100%">
-                        <div class="middle">
-                        <div class="text"><a href='dogdetail.html'>View</a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-sm-6 col-xs-4" style="margin-bottom:20px;margin-right: 0px;margin-left: 0px;">
-                    <div class="containerimg" style="width:100%">
-                        <img src="{{asset('build/images/dog4.jpg') }}" alt="Avatar" class="image" style="width:100%">
-                        <div class="middle">
-                        <div class="text"><a href='dogdetail.html'>View</a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-sm-6 col-xs-4" style="margin-bottom:5px">
-                    <div class="containerimg" style="width:100%">
-                        <img src="{{asset('build/images/dog1.jpg') }}" alt="Avatar" class="image" style="width:100%">
-                        <div class="middle">
-                        <div class="text"><a href='dogdetail.html'>View</a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-sm-6 col-xs-4" style="margin-bottom:5px">
-                    <div class="containerimg" style="width:100%">
-                        <img src="{{asset('build/images/searchres/fem3.jpg') }}" alt="Avatar" class="image" style="width:100%">
-                        <div class="middle">
-                        <div class="text"><a href='dogdetail.html'>View</a></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-      </div>
-  
     </div>
 </div>
 
