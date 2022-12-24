7f628fa07157fe3c1ff079c7e49b3c025e6a9c3c
harvz47 <86585814+harvz47@users.noreply.github.com>
2022-12-24 20:19:33 +0800

    Update editdog.blade.php

--- resources/views/pages/editdog.blade.php
+++ resources/views/pages/editdog.blade.php
@@ -5,15 +5,13 @@
 
 <div class="container-fluid d-flex justify-content-center" style="padding-left: 5%; padding-right: 5%; padding-top:0px;margin-bottom: 20px">
     <div class="row" style="width:100%; margin-top:0px; margin-bottom: 20px;">
-
       <!--Dog profile pic and social media actions -->  
       <div class="col-lg-4 col-sm-6" style="margin-bottom:10px">
         <img src="{{asset('build/images/searchres/fem8.jpg') }}" alt="dog" class="image" style="width:100%;display:block;border-radius: 20px;margin-bottom:10px;margin-top:10px">
         <div style="text-align: center;">
-          <button class="btn-primary3" type="submit" >Remove Uploaded Image</i></button>
+          <button class="btn-primary border rounded-1 px-3 pb-2" type="submit" >Remove Uploaded Image</i></button>
         </div>
       </div>
-
       <!--Dog profile information -->
       <div class="col-lg-5 col-sm-6" style="padding-left:20px;padding-top:15px;margin-bottom: 20px;">
         <h5>Female, 3 mo. old<br>
