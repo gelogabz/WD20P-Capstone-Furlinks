8b319ea8a82c92bbf236d329645323c4316d7e50
Vien08 <113112152+Vien08@users.noreply.github.com>
2022-12-29 21:28:27 +0800

    Add login and signup to home of users

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -56,7 +56,7 @@
       <ul class='list-unstyled'>
           <li class="nav-item dropdown">
                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                <span>Welcome<span> {{ Auth::user()->name }}
+                <span>Welcome !<span> {{ Auth::user()->name }}
               </a>
 
               <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
