2ccd152c195df1dc548cad2bc9fc39c3754e2e58
owhaitchdee <108674156+owhaitchdee@users.noreply.github.com>
2023-01-05 22:31:23 +0800

    fixed drag and drop for create

--- resources/views/dogprofile/createprofile.blade.php
+++ resources/views/dogprofile/createprofile.blade.php
@@ -1,35 +1,119 @@
 @extends ('components.navbar')
 @section('content')
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+{{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-<hr style="margin:0px 0px 5px 0px;padding:0px 0px 0px 0px;border-color:#ececec">
-
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-<script src="https://kit.fontawesome.com/b63f0cdae2.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/b63f0cdae2.js" crossorigin="anonymous"></script> --}}
+<style>
+  .file-upload-input {
+      position: absolute;
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      height: 100%;
+      outline: none;
+      opacity: 0;
+      cursor: pointer;
+      }
 
+  .image-upload-wrap {
+      margin-top: 0px;
+      border: 4px dashed #5082B7 !important ;
+      position: relative;
+      text-align: center;
+      display:block;
+      margin: auto;
+      }
+
+  .image-dropping,
+  .image-upload-wrap:hover {
+      background-color:#cfe5fd !important ;
+      }
+
+  .image-title-wrap {
+      padding: 15px 15px 15px 15px;
+      text-align: center;
+      }
+
+  .drag-text {
+      text-align: center;
+      }
+
+  .drag-text h3 {
+      font-weight: 100;
+      color: #5082B7 !important ;
+      padding: 60px 0;
+      }
+
+  .file-upload-image {
+      max-height: 350px;
+      max-width: 350px;
+      margin: 0px;
+      text-align: center;
+      display:block;
+      margin: auto;
+      border: 4px #5082B7 !important ;
+      }
+  .remove-image {
+      height: 40px;
+      width: 150px;
+      border-radius: 12px;
+      background-color: #799FC8 ;
+      border-color:#5082B7 !important ;
+      color:#F9F9F9;
+      text-decoration: none;
+      font-family: 'Lato', sans-serif;
+      margin:5px;
+      vertical-align: middle;
+      }
+
+  .remove-btn,
+  .file-upload-none{
+      display: none;
+  }
+
+  .remove-image:hover {
+      background-color:#6388af !important ;
+      transition: all .5s ease;
+      -webkit-transition: all .5s ease;
+      -moz-transition: all .5s ease;
+      -o-transition: all .5s ease;
+      -ms-transition: all .5s ease;
+      }
+
+  .remove-image:active {
+      border: 0;
+      transition: all .2s ease;
+      }
+
+</style>
+
+
+<hr style="margin:0px 0px 5px 0px;padding:0px 0px 0px 0px;border-color:#ececec">
 <div class="container-fluid d-flex justify-content-center" style="padding-left: 5%; padding-right: 5%; padding-top:0px;margin-bottom: 20px">
-    <div class="row" style="width:100%; margin-top:0px; margin-bottom: 20px;">
+    <div class="row" style="width:100%;margin-top:20px">
       <h3>Post a dog for adoption</h3><br>
       <form action="{{ route('dogprofile.store') }}" method="POST" enctype="multipart/form-data">
         {!! csrf_field() !!}
       
-      <div class="row" style="width:100%; margin-top:0px; margin-bottom: 20px;">
+      <div class="row" style="width:100%">
       <!--Dog profile pic and social media actions -->  
       <div class="col-lg-4 col-sm-6" style="margin-bottom:10px">
-        <div class="form-group mb-2" style="padding-top:3px;padding-bottom:3px">
-          <label for="pic" style="color:white; align-text:center">Upload your picture:</label> 
-          <div class="image-title-wrap justify-content-center"></div>
-          <div class="image-upload-wrap justify-content-center" style="height:250px;width:250px">
+        <div class="form-group mb-2" style="padding-top:30px;padding-bottom:3px">
+     
+          <div class="image-upload-wrap justify-content-center" style="height:350px;width:350px">
               <input class="file-upload-input" type='file' onchange = "readURL(this);" accept="image/*" id="pic" name="pic" required/>
               <div class="drag-text" style="padding-top:30%">
-              <i class="fa-solid fa-photo-film" style="font-size:50px;color:#581441"></i><br><br><h6>Drag and drop a file <br>or click to browse</h5>
+              <i class="fa-solid fa-photo-film" style="font-size:50px;color:#5082B7;"></i><br><br><h6>Drag and drop a file <br>or click to browse</h5>
               </div>
           </div>
           <div class="file-upload-content">
               <img class="file-upload-none" id="imgdisplay" src="#" alt="your image" />
                   <div class="container justify-content-center">
+                    <div class="image-title-wrap justify-content-center"></div>
+                    <center>
                       <button id="rembutton" type="button" onclick="removeUpload()" class="remove-btn" style="text-align:center;width:250px">Remove selected image</button>
               </div>
           </div>
@@ -37,7 +121,7 @@
       </div>
 
       <!--Dog profile information -->
-      <div class="col-lg-5 col-sm-6" style="padding-left:20px;padding-top:15px;margin-bottom: 20px;">
+      <div class="col-lg-6 col-sm-6" style="padding-left:20px;padding-top:15px;margin-bottom: 20px;">
 
           @if ($errors->any())
           <div class="alert alert-danger">
@@ -49,7 +133,7 @@
               </ul>
           </div>
           @endif
-      =
+
         <div class="container">
             {{-- PATCH- specific part PUT - whole resource --}}
             <div class="mb-2 row">
@@ -66,8 +150,8 @@
             <div class="mb-2 row">
               <label for="age" class="col-sm-4 col-form-label">Age</label>
               <div class="col-sm-4">
-                <input type="number" name="age_yr" class="form-control form-control-sm">
-                <label for="age_yr" class="col-form-label col-form-label-sm" style="font-size:.8rem; font-style:italic">Year/s</label>
+                  <input type="number" name="age_yr" class="form-control form-control-sm">
+                  <label class="form-label-inline" style="font-size:.8rem; font-style:italic">Year/s</span>
               </div>
               <div class="col-sm-4">
                 <input type="number" name="age_month" class="form-control form-control-sm">
@@ -103,7 +187,7 @@
             </div>
 
             <div class="mb-2 row">
-              <label for="breed_id2" class="col-sm-4 col-form-label">Primary Breed</label>
+              <label for="breed_id2" class="col-sm-4 col-form-label">Secondary Breed</label>
               <div class="col-sm-8">
                 <select class="form-select form-select-sm" name="breed_id2" aria-label=".form-select-sm example" required>
                   <option selected>Tap to select</option>
@@ -227,7 +311,6 @@
               </div>
             </div>
 
-
             <input type="submit" name="submit" class="btn btn-primary" value="Submit">
           </form>
           </div>
@@ -241,7 +324,6 @@
     function readURL(input) {
         if (input.files && input.files[0]) {
             let reader = new FileReader();
-
             reader.onload = function(e) {
             document.getElementById("imgdisplay").className = "file-upload-image";
             $('.image-upload-wrap').hide();
