6575f41a5a1fae9be57273c9dc983a02217ed1a5
Margus Odette Alovera <113756701+mowaaah@users.noreply.github.com>
2023-01-07 22:23:37 +0800

    update dogcontroller

--- app/Http/Controllers/DogsController.php
+++ app/Http/Controllers/DogsController.php
@@ -39,4 +39,42 @@ class DogsController extends Controller
         return view('welcome')->with('dogs', $dogs);
     }
     
+    /**
+     * Show the form for creating a new resource.
+     **/
+    public function show($id)
+    {
+        $singleDog = DB::table('dogs')
+            ->select(
+                'dogs.id',
+                'dogs.created_at',
+                'dogs.name',
+                'dogs.gender',
+                'dogs.age_yr',
+                'dogs.age_month',
+                'dogs.breed_id1',
+                'breed1.name as breed1_name',
+                'dogs.breed_id2',
+                'breed2.name as breed2_name',
+                'dogs.pic',
+                'dogs.size',
+                'dogs.color',
+                'dogs.location',
+                'dogs.neutered',
+                'dogs.birthdate',
+                'dogs.rescued',
+                'dogs.rescuedate',
+                'dogs.fee',
+                'dogs.feenotes',
+                'dogs.status_id',
+                'status.name as status_name',
+            )
+            ->join('breed as breed1', 'breed1.id', '=', 'dogs.breed_id1')
+            ->join('breed as breed2', 'breed2.id', '=', 'dogs.breed_id2')
+            ->join('status', 'status.id', '=', 'dogs.status_id')
+            ->where('dogs.id', $id)
+            ->first();
+        return view('pages.dogdetailspublic')->with('dogs', $singleDog);
+    }
+    
 }
