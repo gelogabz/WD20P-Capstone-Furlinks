cd3cae56dd6f5326880599c68646596aa47ef791
owhaitchdee <108674156+owhaitchdee@users.noreply.github.com>
2023-01-10 21:02:37 +0800

    updated dogdetails UI

--- resources/views/dogprofile/editdog.blade.php
+++ resources/views/dogprofile/editdog.blade.php
@@ -106,12 +106,13 @@
   }
 </style>
 
+<hr style="margin:0px 0px 5px 0px;padding:0px 0px 0px 0px;border-color:#ececec">
 <div class="container-fluid d-flex justify-content-center" style="padding-left: 5%; padding-right: 5%; padding-top:0px;margin-bottom: 20px">
-  <div class="row" style="width:100%;"">
+  <div class="row" style="width:100%;margin-top:5px;margin-bottom:20px">
     <a class="btn btn-outline-primary2" href="{{ url()->previous() }}" type="button" style="vertical-align: bottom;text-align: left;padding-left:10px;width:180px;margin-bottom:20px">
       <i class="fa fa-arrow-left" aria-hidden="true" style="font-size:medium;padding-right:10px;padding-top:4px"></i>Back to Dog Profile</a>
 
-    <h3>Edit dog details</h3><br>
+      <h3 style="font-family: Poppins; color:#413F42;">Edit Dog Information</h3><br>
       <form action="{{route('dogprofile.update', $dogs->id)}}" method="POST" enctype="multipart/form-data">
         {!! csrf_field() !!}
       @method('PATCH')
