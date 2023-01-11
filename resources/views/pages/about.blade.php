8ce420db556c16e2b7aca608dffcf3c28c185bac
Margus Odette Alovera <113756701+mowaaah@users.noreply.github.com>
2023-01-11 14:19:08 +0800

    UI about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,10 +1,13 @@
 @extends ('components.navbar')
-
 @section('content')
-    <div class='row;'>
-        <div class="col-lg-6 col-md col-sm">
-            <h1 class="display-3" style="color:#413F42;">ABOUT FURLINKS</h1>
-            <p>
+<div style="background-color: #F4f4f4;">
+    <div class="row align-items-center d-flex my-5 mx-0">
+        <div class="col-sm-6">
+            <img src="{{asset('build/images/about-logo.png') }}" class="rounded-circle my-4 ms-5" style="width: 75%;">
+        </div>
+        <div class="col-md-6">
+            <h1 class="display-3 text-start me-5" style="color:#413F42; font-family:Quicksand; font-weight:400;">ABOUT FURLINKS</h1>
+            <p class="text-start me-5" style="font-family: Lato; font-weight:400;">
                Dog adoption is a wonderful way to bring a new companion into your life and 
                to make a difference in the life of a dog. When you adopt a dog, you are saving 
                a life and providing a home for a dog who may have otherwise been left in a shelter 
@@ -30,8 +33,6 @@
                for a dog, adoption is a great option to consider.  
             </p>
         </div>
-        <div class="col-lg-6 col-md col-sm">
-            <img src="" class="rounded-circle" alt="">
-        </div>
     </div>
+</div>
 @endsection
