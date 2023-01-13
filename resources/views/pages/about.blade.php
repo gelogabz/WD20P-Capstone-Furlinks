b4531d4716633e4400fc565b14adf7e6cc610642
owhaitchdee <108674156+owhaitchdee@users.noreply.github.com>
2023-01-13 14:53:08 +0800

    moved Testimonials to welcome

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,26 +2,25 @@
 @section('content')
 <div style="background-color: #F4f4f4;">
     <div class="row align-items-center d-flex mx-0">
-        <div class="col-sm-6 my-5">
+        <div class="col-sm-5 my-5">
             <img src="{{asset('build/images/about-logo.png') }}" class="rounded-circle ms-5" style="width: 90%;">
         </div>
-        <div class="col-md-6 my-5">
-            <h1 class="display-4 text-center mx-5" style="color:#413F42; font-family:Quicksand; font-weight:800;">ABOUT FURLINKS</h1>
+        <div class="col-md-7 my-5">
+            <h3 class="display-5 text-center mx-5" style="color:#413F42; font-family:Quicksand; font-weight:700;">ABOUT FURLINKS</h3>
             <br>
-            <p class="text-center mx-5" style="font-family: Lato; font-weight:600; color:#413F42;">
+            <h5 class="text-center">Furlinks is a platform created to support the 'Adopt, Don't Shop' dogs campaign. </h5><br>
+            <p class="text-center mx-5" style="font-family: Lato; font-weight:500; color:#413F42;">
                Dog adoption is a wonderful way to bring a new companion into your life and 
                to make a difference in the life of a dog. When you adopt a dog, you are saving 
                a life and providing a home for a dog who may have otherwise been left in a shelter 
-               or rescue. There are many benefits to adopting a dog, and it is a responsible and 
-               rewarding decision.
+               or rescue. 
                <br><br>
                When you adopt a dog, you are giving a dog a second chance at 
                a happy life. Many dogs in shelters and rescues have been abandoned, abused, or 
                neglected by their previous owners. By adopting a dog, you are providing a loving 
                and stable home for a dog who may have never known love or stability before.
                <br><br>
-               In addition to saving a life and providing a loving home, there are many other 
-               benefits to adopting a dog. Adopting a dog can be less expensive than purchasing 
+               Adopting a dog can be less expensive than purchasing 
                a dog from a breeder or pet store, as the adoption fee often includes the cost 
                of spaying or neutering, vaccinations, and other veterinary care. Adopting a 
                dog can also be more rewarding, as you are giving a dog a second chance and 
